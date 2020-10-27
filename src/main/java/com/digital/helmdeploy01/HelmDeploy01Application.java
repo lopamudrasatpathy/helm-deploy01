42b3b67322e657575f1fe644088cd58dcaf6e4e6
lopamudrasatpathy <lopamudra.satpathy@gmail.com>
2020-10-27 10:30:38 -0500

    Initial version

---
+++ src/main/java/com/digital/helmdeploy01/HelmDeploy01Application.java
@@ -0,0 +1,13 @@
+package com.digital.helmdeploy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelmDeploy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelmDeploy01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/helmdeploy01/HelmDeploy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.helmdeploy01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelmDeploy01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
